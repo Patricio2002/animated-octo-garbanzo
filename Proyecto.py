b6c8b55bff5a3619df25705a163381d919dea1e1
SaChHe <samuel.chong@ingenieria.unam.edu>
2023-02-22 13:28:55 -0600

    Comienzo en creación de clases

--- Proyecto.py
+++ Proyecto.py
@@ -1,13 +1,15 @@
-class maquinaExpendedora():     #Clase principal
-    def __init__():
-        pass
-
+#Ideas jeje
 class producto():                           #Clase padre
     def __init__(self, Producto, Marca):
         pass
 
-class Papas(producto):
+class Papas(producto):          
     pass
+
+class maquinaExpendedora():     #Clase principal
+    elemento = producto("Papas", "Sabritas")
+    def __init__(elemento):
+        pass
 
     
 
