afc2d8c4c965ca4b64fbea98f6d52a2b345527f5
SaChHe <samuel.chong@ingenieria.unam.edu>
2023-02-22 13:24:58 -0600

    Comienzo en creación de clases

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,19 @@
-class maquinaExpendedora():
+class maquinaExpendedora():     #Clase principal
     def __init__():
         pass
 
+class producto():                           #Clase padre
+    def __init__(self, Producto, Marca):
+        pass
+
+class Papas(producto):
+    pass
+
+    
+
+#crear clase padre "producto" para usarse en clases hijas como refresco, papas, etc.
+#que contenga marca, sabor, etc
+
+#diccionario para simular tablero de la maquina expendedora 
+#hacer lista de productos
+ 
