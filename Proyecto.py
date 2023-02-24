931e3a3944b6dcef530f4aed2f64269462642014
Patricio2002 <patricio.ad26301@gmail.com>
2023-02-24 11:08:35 -0600

    opcion iniciar sesion colocada

--- Proyecto.py
+++ Proyecto.py
@@ -15,13 +15,28 @@ def productosTipo():
 
 def infoProductos():
     pass
+
+def iniciarSesion():
+    usuario = input("Ingrese su usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+    Administrador(usuario, contraseña)
+
+    validar = Administrador.validacion()
+
+    if validar:
+        print(f"Bienvenido {usuario}")
+        opcion = input("¿Que desea hacer?\n\
+1. Añadir nuevo producto\n\
+2. Eliminar producto\n\
+3. Modificar informaciónd de un producto\n\
+4. Cerrar sesión")
  
 
 def main():
     opcion = int(input("¿Qué es lo que desea hacer?\n\
 1. Seleccionar producto\n\
-2. Almacenar nuevo producto\n\
 3. Enlistar total de productos de un tipo\n\
 4. Enlistar todos los productos\n\
 5. Ver informacion de un producto\n\
+2. Iniciar sesión (Administrador)\n\
     "))
--- admin.py
+++ admin.py
@@ -1,10 +1,7 @@
 class Administrador():
-    def __init__(self, nombre, contraseña) -> None:
-        self.nombre = nombre
+    def __init__(self, nombre: str, contraseña: str) -> None:
+        self.__nombre = nombre
         self.__contraseña = contraseña
 
-    def bienvenida(self):
-        print(f"Bienvenido {self.nombre}")
-    
-    def datoInvalido(self):
-        print("Usuario o contraseña incorrectos")
+    def validacion(self):
+        pass
