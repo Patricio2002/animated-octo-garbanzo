90f1e5b90a46c0c1e86ea8d7f6a8740903a22fa3
SaChHe <samuel.chong@ingenieria.unam.edu>
2023-03-02 15:24:44 -0600

    EXCEPCIONES Y SELECCION DE ARTICULO

--- Proyecto.py
+++ Proyecto.py
@@ -1,8 +1,21 @@
 from admin import *
 from funcionesAdmin import *
 from funcionesAdmin import iniciarSesion
-#from productos import *
+import time
 maquina = MaquinaExpendedora()
+
+
+def dispensando():
+
+    print("Dispensando...")
+    
+    for i in range(1, 10):
+        #print("[" + "|" * i + "]")
+        time.sleep(0.5)
+        
+    #
+    print()
+    print("Recoja su producto, gracias por comprar :)")
 
         
 def mostrarProductos():
@@ -11,6 +24,7 @@ def mostrarProductos():
 def seleccionarProducto():
     clave = input("Ingrese la clave del producto que desea comprar: ")
     maquina.comprarProductos(int(clave))
+    dispensando()
 
 def productosTipo():
     pass
@@ -25,17 +39,24 @@ def infoProductos():
 def main():
     while 1:
         maquina.mostrarProductos()
-        opcion = int(input("¿Qué es lo que desea hacer?\n\
+        try:
+            opcion = int(input("¿Qué es lo que desea hacer?\n\
   1. Seleccionar producto\n\
   2. Ver informacion de un producto\n\
   3. Modo Administrador\n\
-  4. Salir\n"))
-        if opcion == 1:
-            seleccionarProducto()
-        elif opcion == 3:
-            iniciarSesion()
-        elif opcion == 4:
-            print("Gracias por haber utilizado la maquina expendedora 'Kunkito'")
+  4. Salir\n"
+  ":"))
+            if opcion == 1:
+                seleccionarProducto()
+            elif opcion == 3:
+                iniciarSesion()
+            elif opcion == 4:
+                print("Gracias por haber utilizado la maquina expendedora 'Kunkito'")
             break
+       
+        except ValueError:
+        
+         print("\nIntroduzca solo numeros!!!")
+
 
 main()
